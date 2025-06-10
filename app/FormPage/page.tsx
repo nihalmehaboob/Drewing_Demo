ecd4617efa9012c5b822bf7d15ba4a846486ed9c
Mohamed NIHAL K <115793246+nihalmehaboob@users.noreply.github.com>
2025-06-10 18:38:24 +0530

    supabase connected and elemants are inserted into Users table by creating form

--- app/FormPage/page.tsx
+++ app/FormPage/page.tsx
@@ -1,9 +1,44 @@
 'use client'; 
 import Link from 'next/link';
-export default function ContactForm() {
+import React from 'react'; 
+import { useState } from 'react';
+import supabase from '../../lib/supabase';
+
+
+
+ const ContactForm = ()=> {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  //const [error, setError] = useState('');
+
+ const handlesubmit= async(e: React.FormEvent<HTMLFormElement>)=> {
+    e.preventDefault(); 
+   
+    if (!username || !email) {
+        alert("Please fill all the fields");
+        return;
+    }
+    
+    console.log(username, email);
+    
+    const { error } = await supabase
+        .from('Users')
+        .insert([{ username, email }]);
+    
+    if (error) {
+        alert(error.message);
+    } else {
+        // Success
+        setUsername('');
+        setEmail('');
+        alert("User added successfully!");
+    }
+}
+
   return (
-    <main className="min-h-screen bg-gray-50 dark:bg-gray-900 flex items-center justify-center px-4 py-10">
-      <form className="w-full max-w-md bg-white dark:bg-gray-800 p-8 rounded-lg shadow-lg space-y-6">
+
+    <main className="min-h-screen bg-gray-50 dark:bg-gray-900 flex items-center justify-center px-4 py-1">
+      <form className="w-full max-w-md bg-white dark:bg-gray-800 p-8 rounded-lg shadow-lg space-y-6" onSubmit={handlesubmit}>
         <div>
           <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
             Enter your Name
@@ -11,7 +46,9 @@ export default function ContactForm() {
           <input
             type="text"
             className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white dark:border-gray-600"
-            placeholder="John Doe"
+            placeholder="Mohammed Nihal"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
           />
         </div>
 
@@ -22,7 +59,10 @@ export default function ContactForm() {
           <input
             type="email"
             className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white dark:border-gray-600"
-            placeholder="john@example.com"
+            placeholder="nihal@example.com"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+
           />
         </div>
 
@@ -44,3 +84,5 @@ export default function ContactForm() {
     </main>
   );
 }
+
+export default ContactForm;
