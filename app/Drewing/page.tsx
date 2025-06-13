892bf9aeaf6a52e1435c665acb1788496a8a900b
Mohamed NIHAL K <115793246+nihalmehaboob@users.noreply.github.com>
2025-06-13 20:08:53 +0530

    designed pop up page now working on storing excalidraw json file

--- app/Drewing/page.tsx
+++ app/Drewing/page.tsx
@@ -2,7 +2,36 @@
 import React from 'react';
 import Link from 'next/link';
 import Excalidraw from './Exclidraw';
+import {useState,useRef} from 'react';
+
+
 const DrewPage = () => {
+
+  const [popopen, Setpopopen] = useState(false);
+  const [drawName, SetdrawName] = useState('');
+  const excalidrawRef = useRef(null);
+
+  const handlesubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!drawName) {
+      alert("Please enter a name for your drawing");
+      return;
+    }
+
+    console.log(drawName);
+    const api = excalidrawRef.current;
+    console.log(api)
+    if(api){
+      // const elements = api.getSceneElements();
+      // const appState = api.getAppState();
+      // const files = api.getFiles();
+    }
+
+     alert("User added successfully!");
+     SetdrawName('');
+    Setpopopen(false);
+  }
+
   
   return (
     <main className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col items-center justify-center px-4">
@@ -14,16 +43,49 @@ const DrewPage = () => {
         <Excalidraw />
       </div>
         
-       
+      {popopen && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+          <div className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg">
+            <h2 className="text-xl font-semibold mb-4">Save Your Drawing</h2>
+           <div>
+            <form onSubmit={handlesubmit} className="space-y-4">
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-700 dark:text-gray-300">Draw Name</label>
+              <input type= "text" name="Draw_name" onChange={(e)=>SetdrawName(e.target.value)}  className="w-full px-4 py-2  mb-2 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-white dark:border-gray-600"></input>
+            </div>
+            <div className='flex px-20 '>
+           <button 
+              className="px-4 py-2 mx-2 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-lg shadow transition duration-300"
+              onClick={() => Setpopopen(false)}
+            >
+              Close
+            </button>
+           <button 
+              className="px-4 py-2 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-lg shadow transition duration-300"
+             
+              type='submit'
+            >
+              Save
+            </button>
+            
+            </div>
+            </form>
+           </div>
+
+           
+          </div>
+        </div>  
+      )}
       </div>
       <div className='flex mx-10'>  
       <Link href="/">
         
-              <button className="mt-4 px-6 py-3 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-lg shadow transition duration-300">
+          <button className="mt-4 px-6 py-3 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-lg shadow transition duration-300">
             Go to Home
           </button>
            </Link>
-          <button className="mt-4 px-6 py-3 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-lg shadow transition duration-300">
+          <button className="mt-4 px-6 py-3 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-lg shadow transition duration-300" 
+           onClick={()=>Setpopopen(true)}>
            Save
           </button>
        
